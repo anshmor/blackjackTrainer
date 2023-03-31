bcd84a7acf4f99f422da75aaf0109ba3d649541c
anshmor <ansh.moraje@gmail.com>
2023-03-30 21:37:32 -0500

    cards showing up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
+import Hand from './Hand.js';
 
 function App() {
+  var suits = ["spade", "diamond", "club", "heart"];
+  var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  function getDeck() {
+    let deck = [];
+    let howManyDecks = 1;
+
+    for(let i = 0; i < suits.length * howManyDecks; i++)
+    {
+      for(let x = 0; x < values.length; x++)
+      {
+        let card = {Value: values[x], Suit: suits[i]};
+        deck.push(card);
+      }
+    }
+
+    return deck;
+  }
+
+  function shuffle(deck) {
+    // for 1000 turns
+    // switch the values of two random cards
+    for (let i = 0; i < 1000; i++)
+    {
+      let location1 = Math.floor((Math.random() * deck.length));
+      let location2 = Math.floor((Math.random() * deck.length));
+      let tmp = deck[location1];
+
+      deck[location1] = deck[location2];
+      deck[location2] = tmp;
+    }
+  }
+
+  let deck = getDeck();
+  shuffle(deck);
+
+
   return (
-    <div>Hello World</div>
+    <Hand deck={deck}></Hand>
   );
 }
 
--- frontend/src/Hand.js
+++ frontend/src/Hand.js
@@ -1,7 +1,24 @@
+import {useState } from 'react'
+import { Image, Card, Container } from 'react-bootstrap';
 import './App.css';
 
 function Hand(props) {
-  
+    const [cards, setCards] = useState([props.deck.pop(), props.deck.pop(), props.deck.pop(), props.deck.pop()]);
+
+    return (
+        <div className="card-stack" style={{ '--num-cards': cards.length }}>
+            {cards.map((card, index) => {
+                return (
+                    <Card className="my-card" style={{ '--card-index': index + 1 }}>
+                        <Card.Body>
+                            <Card.Text>{card["Value"]}</Card.Text>
+                            <Image className="suit" src={card["Suit"] + ".png"}></Image>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+          </div>
+    )
 }
 
 export default Hand;
